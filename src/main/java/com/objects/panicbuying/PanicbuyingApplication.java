60d778339d73ac0e36626e9f2fa2b21fcb558850
Minwoo Jhung <minwoorich@naver.com>
2023-12-29 16:22:48 +0900

    initial commit

---
+++ src/main/java/com/objects/panicbuying/PanicbuyingApplication.java
@@ -0,0 +1,13 @@
+package com.objects.panicbuying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanicbuyingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanicbuyingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/objects/panicbuying/PanicbuyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objects.panicbuying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanicbuyingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
